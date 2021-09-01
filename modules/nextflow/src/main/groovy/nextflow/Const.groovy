7d9715f8bdef5ae5066d1e6ea86af7b0b0f7b136
Nicolas Josef Zunker <n.zunker@campus.tu-berlin.de>
2021-09-01 14:05:56 +0000

    log taskrun data in vanilla setup with rl_active set to false

--- modules/nextflow/src/main/groovy/nextflow/Const.groovy
+++ modules/nextflow/src/main/groovy/nextflow/Const.groovy
@@ -58,12 +58,12 @@ class Const {
     /**
      * The app build time as linux/unix timestamp
      */
-    static public final long APP_TIMESTAMP = 1627415207112
+    static public final long APP_TIMESTAMP = 1630505125068
 
     /**
      * The app build number
      */
-    static public final int APP_BUILDNUM = 5552
+    static public final int APP_BUILDNUM = 5556
 
 
     /**
